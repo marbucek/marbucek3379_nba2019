9158a1a76e013f6419e5f715572ba594fc319ca2
Martin Bucháček <bucmarti@phys.ethz.ch>
2020-01-19 19:33:27 +0100

    code rewritten in object oriented way

---
+++ models.py
@@ -0,0 +1,168 @@
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+import math
+import statsmodels.api as sm
+
+
+def home_win_probability(home_elo, away_elo,
+                         K, home_advantage # source: 538
+                        ):
+    return 1 / (1 + math.pow(K, -(home_advantage+home_elo - away_elo) / 400))
+
+
+## Get new Elo ratings home and away teams after a game
+def get_updated_elo(
+    home_elo, away_elo,
+    point_diff, ## > 0 if home team wins, < 0 if away team wins, cannot be 0 (will throw error)
+    K,  ## model hyperparameter
+    home_advantage,
+    use_margin
+):
+
+    P_home_win = home_win_probability(home_elo, away_elo, K, home_advantage)
+    P_away_win = 1 - P_home_win
+
+    # When home team wins
+    if point_diff > 0 :
+        elo_diff = home_elo-away_elo
+        M = (point_diff+3)**0.8/(7.5+0.006*elo_diff) if use_margin else 1 # multiplier, source 538
+        home_elo += M * K * P_away_win
+        away_elo -= M * K * P_home_win
+
+    # When away team wins
+    elif point_diff < 0 :
+        elo_diff = away_elo-home_elo
+        M = (-point_diff+3)**0.8/(7.5+0.006*elo_diff) if use_margin else 1 # multiplier, source 538
+        home_elo -= M * K * P_away_win
+        away_elo += M * K * P_home_win
+
+    else : raise ValueError(f"point_diff should be non-zero number. Got {point_diff}")
+
+    return home_elo, away_elo
+
+
+## Iterate through games updating each teams Elo rating
+def get_elos_over_time(data,  ## dataframe of games, must be in order of occurence
+                       starting_elo_dict={},  ## dictionary of elo scores by team at the beginning of the data period
+                       default_elo=0,  ## elo initally given to a team not in starting_elo_dict
+                       K=10,
+                       home_advantage=100,
+                       use_margin=True## model hyperparameter; higher number means individuals game affects Elo more
+                       ):
+    elo_dict = starting_elo_dict.copy()
+
+    ## Iterate over rows of the dataframe (i.e. over games)
+    for i, row in data.iterrows():
+        home_team = row['homeTeam']
+        away_team = row['awayTeam']
+        home_elo = elo_dict.get(home_team, default_elo)
+        away_elo = elo_dict.get(away_team, default_elo)
+
+        ## Put the team's current ELO in the dataframe (this is the teams ELO *before* the match)
+        data.loc[i, 'homeElo'] = home_elo
+        data.loc[i, 'awayElo'] = away_elo
+
+        ## Calculate the new elo scores and update elo_dict with them
+        point_diff = row['pointsDiff']
+        home_elo, away_elo = get_updated_elo(home_elo, away_elo, point_diff, K, home_advantage, use_margin)
+        elo_dict[home_team] = home_elo
+        elo_dict[away_team] = away_elo
+
+    return elo_dict
+
+def fit_scores(train_data):
+    train_data['EloDifference'] = train_data['homeElo'] - train_data['awayElo']
+    train_data['EloSum'] = train_data['homeElo'] + train_data['awayElo']
+    X = train_data[['EloDifference', 'EloSum']]
+    X = sm.add_constant(X.values)
+
+    y = train_data['pointsDiff']
+    diff_model = sm.OLS(y, X).fit()
+    y = train_data['pointsSum']
+    sum_model = sm.OLS(y, X).fit()
+
+    return diff_model, sum_model
+
+def predict_scores(required_predictions, elo_dict, diff_model, sum_model):
+    tmp = required_predictions[['homeTeam', 'awayTeam']].copy()
+    tmp['homeElo'] = [elo_dict.get(team,0) for team in tmp['homeTeam']]
+    tmp['awayElo'] = [elo_dict.get(team,0) for team in tmp['awayTeam']]
+    tmp['EloDifference'] = tmp.eval('homeElo - awayElo')
+    tmp['EloSum'] = tmp.eval('homeElo + awayElo')
+    X = tmp[['EloDifference', 'EloSum']]
+    X = sm.add_constant(X.values)
+    tmp['predictedDiff'] = diff_model.predict(X)
+    tmp['predictedSum'] = sum_model.predict(X)
+
+    required_predictions['predictedDiff'] = tmp['predictedDiff']
+    required_predictions['predictedSum'] = tmp['predictedSum']
+
+class ELO():
+    def __init__(self, K=10, home_advantage=0, use_margin=False, lag = -1, reset_after_season=False):
+        """
+        reset_evolve     resetting ELO dictionary in the beginning of a new seasons
+        """
+        self.data = None
+        self.ELO = defaultdict(float)
+        self.K = K
+        self.home_advantage = home_advantage
+        self.use_margin = use_margin
+        self.reset_after_season = reset_after_season
+
+        if not (lag == -1 or lag > 0):
+            raise Exception('Lag parameter must be either -1 or positive integer')
+        self.lag = lag
+
+    def load_data(self, data):
+        self.data = data
+
+    def add_data(self, data):
+        self.data = pd.concat([self.data, data], sort=False)
+        #if (self.lag != -1) and (len(self.data['week'].unique()) > self.lag):
+        #    self.data = self.data[self.data['week'] > self.data['week'].min()]
+        #else:
+        #    self.data = self.data[self.data['season'] == season]
+
+    def evolve(self, weeks='last'):
+        if 'homeElo' not in self.data.columns:
+            self.data.loc[:,'homeElo'] = np.nan
+            self.data.loc[:,'awayElo'] = np.nan
+
+        if weeks == 'last':
+            last_week = self.data[self.data['week'] == self.data['week'].max()]
+            previous_weeks = self.data[self.data['week'] < self.data['week'].max()]
+            self.ELO = get_elos_over_time(last_week,
+                                          starting_elo_dict=self.ELO,
+                                          K=self.K, home_advantage=self.home_advantage, use_margin=self.use_margin)
+            self.data = pd.concat([previous_weeks, last_week], sort=False)
+
+        if weeks == 'current_season':
+            evolve_data = self.data[self.data['season'] == self.data['season'].max()]
+            self.ELO = get_elos_over_time(self.data,
+                                          starting_elo_dict={},
+                                          K=self.K, home_advantage=self.home_advantage, use_margin=self.use_margin)
+
+        if weeks == 'all':
+            self.ELO = get_elos_over_time(self.data,
+                                          starting_elo_dict={},
+                                          K=self.K, home_advantage=self.home_advantage, use_margin=self.use_margin)
+
+
+
+    def reset_evolve(self):
+        if self.reset_after_season:
+            self.ELO = defaultdict(float)
+
+    def fit(self):
+        if self.lag == -1:
+            current_season = self.data['season'].max()
+            fit_data = self.data[self.data['season'] == current_season]
+        else:
+            weeks = sorted(self.data['week'].unique(), reverse=True)[0:self.lag]
+            fit_data = self.data[self.data['week'].isin(weeks)]
+        self.diff_model, self.sum_model = fit_scores(fit_data)
+        self.diff_model, self.sum_model = fit_scores(fit_data)
+
+    def predict(self, required_predictions):
+        predict_scores(required_predictions, self.ELO, self.diff_model, self.sum_model)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import statsmodels.api as sm
 import numpy as np
-import math
+from models import ELO
 
 # This is the main function you are requested to define.
 # required_predictions: a list of matches that you should predict
@@ -52,122 +52,25 @@ def get_multi_season_game_data(data_loader, first_year, last_year):
     data.reset_index(inplace=True, drop=True)
     return data
 
-
-## Elo model's probability of home team winning
-def home_win_probability(home_elo, away_elo,
-                         home_advantage = 100 # source: 538
-                        ):
-    return 1 / (1 + math.pow(10, -(home_advantage+home_elo - away_elo) / 400))
-
-
-## Get new Elo ratings home and away teams after a game
-def get_updated_elo(
-    home_elo, away_elo,
-    point_diff, ## > 0 if home team wins, < 0 if away team wins, cannot be 0 (will throw error)
-    K,  ## model hyperparameter
-):
-
-    P_home_win = home_win_probability(home_elo, away_elo)
-    P_away_win = 1 - P_home_win
-
-    # When home team wins
-    if point_diff > 0 :
-        elo_diff = home_elo-away_elo
-        M = (point_diff+3)**0.8/(7.5+0.006*elo_diff)# multiplier, source 538
-        home_elo += M * K * P_away_win
-        away_elo -= M * K * P_home_win
-
-    # When away team wins
-    elif point_diff < 0 :
-        elo_diff = away_elo-home_elo
-        M = (-point_diff+3)**0.8/(7.5+0.006*elo_diff)# multiplier, source 538
-        home_elo -= M * K * P_away_win
-        away_elo += M * K * P_home_win
-
-    else : raise ValueError(f"point_diff should be non-zero number. Got {point_diff}")
-
-    return home_elo, away_elo
-
-
-## Iterate through games updating each teams Elo rating
-def get_elos_over_time(data,  ## dataframe of games, must be in order of occurence
-                       starting_elo_dict={},  ## dictionary of elo scores by team at the beginning of the data period
-                       default_elo=0,  ## elo initally given to a team not in starting_elo_dict
-                       K=20,  ## model hyperparameter; higher number means individuals game affects Elo more
-                       ):
-    elo_dict = starting_elo_dict.copy()
-    data['homeElo'] = np.nan
-    data['awayElo'] = np.nan
-
-    ## Iterate over rows of the dataframe (i.e. over games)
-    for i, row in data.iterrows():
-        home_team = row['homeTeam']
-        away_team = row['awayTeam']
-        home_elo = elo_dict.get(home_team, default_elo)
-        away_elo = elo_dict.get(away_team, default_elo)
-
-        ## Put the team's current ELO in the dataframe (this is the teams ELO *before* the match)
-        data.loc[i, 'homeElo'] = home_elo
-        data.loc[i, 'awayElo'] = away_elo
-
-        ## Calculate the new elo scores and update elo_dict with them
-        point_diff = row['pointsDiff']
-        home_elo, away_elo = get_updated_elo(home_elo, away_elo, point_diff, K)
-        elo_dict[home_team] = home_elo
-        elo_dict[away_team] = away_elo
-
-    return elo_dict
-
-def fit_scores(train_data):
-    train_data['EloDifference'] = train_data['homeElo'] - train_data['awayElo']
-    train_data['EloSum'] = train_data['homeElo'] + train_data['awayElo']
-
-    #print('Fitting linear model from Elo difference and sum to points difference')
-    X = train_data[['EloDifference', 'EloSum']]
-    X = sm.add_constant(X)
-    y = train_data['pointsDiff']
-    diff_model = sm.OLS(y, X).fit()
-
-    #print('Fitting linear model from Elo difference and sum to points sum')
-    y = train_data['pointsSum']
-    sum_model = sm.OLS(y, X).fit()
-
-    return diff_model, sum_model
-
-def predict_scores(required_predictions, elo_dict, diff_model, sum_model):
-
-    #print('Generating predictions')
-    #     required_predictions = pd.DataFrame(required_predictions)
-    tmp = required_predictions[['homeTeam', 'awayTeam']].copy()
-    tmp['homeElo'] = [elo_dict.get(team,0) for team in tmp['homeTeam']]
-    tmp['awayElo'] = [elo_dict.get(team,0) for team in tmp['awayTeam']]
-    tmp['EloDifference'] = tmp.eval('homeElo - awayElo')
-    tmp['EloSum'] = tmp.eval('homeElo + awayElo')
-    X = tmp[['EloDifference', 'EloSum']]
-    X = sm.add_constant(X)
-    tmp['predictedDiff'] = diff_model.predict(X)
-    tmp['predictedSum'] = sum_model.predict(X)
-
-    required_predictions['predictedDiff'] = tmp['predictedDiff']
-    required_predictions['predictedSum'] = tmp['predictedSum']
-
-    #print('Finished')
-
-
 def predict(required_predictions, data_loader, first_season=2009, last_season=2020):
 
     print('Loading training data')
     train_data = get_multi_season_game_data(data_loader, first_season, last_season)
-    current_season = train_data['season'].iloc[-1]
-    train_data = train_data[train_data['season'] == current_season]
+    #
+    #train_data = train_data[train_data['season'] == current_season]
 
-    print('Getting Elo ratings over time on train data')
-    elo_dict = get_elos_over_time(train_data,
-                                  starting_elo_dict={},
-                                  K=10)
+    model = ELO(K=10, home_advantage=0, use_margin=False, lag=-1, reset_after_season=True)
+    model.add_data(train_data)
+    model.evolve(weeks='current_season') #calculate ELO over all weeks available
+    model.fit()
 
-    print('Fitting model')
-    diff_model, sum_model = fit_scores(train_data)
-    predict_scores(required_predictions, elo_dict, diff_model, sum_model)
+    current_season = train_data['season'].max()
+    data_season = train_data[train_data['season'] == current_season]
+    first_match_date = data_season.dateTime.dt.date.min().toordinal()
+    first_monday = first_match_date - (first_match_date - 1) % 7
+    data_season['week'] = train_data['dateTime'].dt.date.apply(lambda x: (x.toordinal() - first_monday)//7)
+    print(data_season['week'])
+    print(f"Calculating predictions for season {current_season}, week {data_season['week'].max() + 1}")
+    model.predict(required_predictions)
 
     return required_predictions
